432e109b7987b17a7a32505da7879bd2d3148825
PyromancerBoom <priyanshmagic@gmail.com>
2022-07-06 16:45:45 +0530

    short url route

--- server/server.js
+++ server/server.js
@@ -1,10 +1,30 @@
 const express = require("express");
 const { mongoose } = require("mongoose");
+const ShortUrl = require("./models/short");
 const app = express();
 
 mongoose.connect("mongodb://localhost/urlShortner", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+});
+
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
+
+app.get("/", async (req, res) => {
+  const shortUrls = await ShortUrl.find();
+});
+
+app.post("/shortUrls", async (req, res) => {
+  await ShortUrl.create({ full: req.body.fullUrl });
+  res.redirect("/");
+});
+
+app.listen(1337, () => {
+  console.log("Server starting on 1337");
 });
 
 // const path = require("path");
@@ -14,9 +34,3 @@ mongoose.connect("mongodb://localhost/urlShortner", {
 // app.get("/", (req, res) => {
 //   res.sendFile(path.join(__dirname, "public", "index.html"));
 // });
-
-app.post("/shortUrls", (req, res) => {});
-
-app.listen(1337, () => {
-  console.log("Server starting on 1337");
-});
